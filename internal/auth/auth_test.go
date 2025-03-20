efd955c8b544de5ef08512b3dcaf409380dc2db3
FrostyRaptor <e1wink@outlook.com>
2025-03-20 17:16:37 -0500

    Added error

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -34,7 +34,7 @@ func TestGetAPIKey(t *testing.T) {
 		{
 			key:       "Authorization",
 			value:     "ApiKey xxxxxx",
-			expect:    "xxxxxx",
+			expect:    "xxxxx",
 			expectErr: "not expecting an error",
 		},
 	}
